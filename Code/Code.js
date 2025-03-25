7aa42bbf4bf1de60630bafa60e68d31a48655930
Johnmc2882 <john14mccarthy@gmail.com>
2025-03-25 19:27:51 +0000

    Add files via upload

--- Code/Code.js
+++ Code/Code.js
@@ -1,16 +1,40 @@
+const pathtowizardImage = "images/Mageborder.png"
+const pathtoThiefImage = "images/Thiefborder.png"
+const pathtoFighterImage = "images/Fighterborder.png"
 function handleSubmit(e){
     e.preventDefault();
     let chosenName = document.querySelector("#name-input").value;
     document.querySelector("#name-output").innerHTML = chosenName;
-    document.querySelector("#sheet").classList.remove("hide");
+
+    let chosenAge = document.querySelector("#Age-input").value;
+    document.querySelector("#Age-output").innerHTML = chosenAge;
+
+    let ChosenClass = document.querySelector("Cl")
 }
 
 
 function ClassSelected(){
     console.log("Changed class");
     let selectedClass = document.querySelector("#ClassSelector").value;
+    if(selectedClass == "Fighter"){
+        document.querySelector("img").src = pathtoFighterImage;
+        document.querySelector("#Stealth").classList.add("hide");
+        document.querySelector("#chooseSpell").classList.add("hide");
+        document.querySelector("#Mana").classList.add("hide");
+        document.querySelector("#chooseWeapon").classList.remove("hide")
+    }
     if(selectedClass == "Mage" ){
-        document.querySelector("#sheet").classList.remove("hide");
         document.querySelector("#chooseSpell").classList.remove("hide");
+        document.querySelector("#Mana").classList.remove("hide");
+        document.querySelector("img").src = pathtowizardImage;
+        document.querySelector("#Stealth").classList.add("hide");
+        document.querySelector("#chooseWeapon").classList.add("hide")
+    }
+    if(selectedClass == "Thief"){
+        document.querySelector("img").src = pathtoThiefImage;
+        document.querySelector("#Stealth").classList.remove("hide");
+        document.querySelector("#chooseSpell").classList.add("hide");
+        document.querySelector("#Mana").classList.add("hide");
+        document.querySelector("#chooseWeapon").classList.add("hide")
     }
 }
